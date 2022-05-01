bd49c8c8ca639e39f76de1e2a8b8ceda2b197951
elzup <guild0105@gmail.com>
2022-05-01 10:49:14 +0900

    change test ts-jest to swc

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,20 @@
-module.exports = {
-  preset: "ts-jest",
-  testEnvironment: "node",
+const config = {
+  testMatch: ["**/*.test.js", "**/*.test.ts", "**/*.test.tsx"],
+  testPathIgnorePatterns: ["<rootDir>/node_modules/", "<rootDir>/dist/"],
+  transformIgnorePatterns: ["/node_modules/"],
+
+  transform: {
+    ".+\\.(t|j)s$": [
+      "@swc/jest",
+      {
+        sourceMaps: true,
+        module: { type: "commonjs" },
+        jsc: {
+          parser: { syntax: "typescript", tsx: true },
+        },
+      },
+    ],
+  },
 }
+
+module.exports = config
